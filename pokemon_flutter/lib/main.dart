76fe37a091806f180eddde9f87152eb57e8be99d
小林正季 <>
2024-02-26 20:27:32 +0900

    不要なコメントを削除

--- pokemon_flutter/lib/main.dart
+++ pokemon_flutter/lib/main.dart
@@ -31,14 +31,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,11 +75,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         )
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
