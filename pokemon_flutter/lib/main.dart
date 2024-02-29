aef8ac27660fe38d2149fe70d7f482631fd14e13
小林正季 <>
2024-02-29 21:07:23 +0900

    Settingsから、画面のモード選択を可能に変更

--- pokemon_flutter/lib/main.dart
+++ pokemon_flutter/lib/main.dart
@@ -156,6 +156,11 @@ class _SettingsState extends State<Settings> {
         ListTile(
           leading: Icon(Icons.lightbulb),
           title: Text('Dark/Light Mode'),
+          onTap: () => {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const ThemeModeSelectionPage(),
+            ))
+          },
         ),
         SwitchListTile(
           title: const Text('Switch'),
@@ -203,6 +208,45 @@ class _SettingsState extends State<Settings> {
           ),
         )
       ],
+    );
+  }
+}
+
+class ThemeModeSelectionPage extends StatelessWidget {
+  const ThemeModeSelectionPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.system,
+              groupValue: ThemeMode.system,
+              title: const Text('System'),
+              onChanged: (newValue) {},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.light,
+              groupValue: ThemeMode.system,
+              title: const Text('Light'),
+              onChanged: (newValue) {},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: ThemeMode.system,
+              title: const Text('Dark'),
+              onChanged: (newValue) {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
