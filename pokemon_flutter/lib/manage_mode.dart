8b99d0bdb9d86d1910b9a0e2de82d400f1989118
小林正季 <>
2024-03-04 20:46:05 +0900

    Shared Preferencesプラグインを利用して、prefにtheme_modeという引数で値を保存する処理を追加

---
+++ pokemon_flutter/lib/manage_mode.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+// ThemeModeを数値に変換
+int modeToVal(ThemeMode mode) {
+  switch(mode) {
+    case ThemeMode.system:
+      return 1;
+    case ThemeMode.dark:
+      return 2;
+    case ThemeMode.light:
+      return 3;
+    default:
+      return 0;
+  }
+}
+
+// 数値をThemeModeに変換
+ThemeMode valToMode(int val) {
+  switch(val) {
+    case 1:
+      return ThemeMode.system;
+    case 2:
+      return ThemeMode.dark;
+    case 3:
+      return ThemeMode.light;
+    default:
+      return ThemeMode.system;
+  }
+}
+
+// 引数に渡されたmodeを、数値としてprefに保存する
+Future<void> saveThemeMode(ThemeMode mode) async {
+  final pref = await SharedPreferences.getInstance();
+  // theme_modeというKeyで、modeの値を渡す
+  pref.setInt('theme_mode', modeToVal(mode));
+}
+
+//
+Future<ThemeMode> loadThemeMode() async {
+  final pref = await SharedPreferences.getInstance();
+  // prefに保存したtheme_modeの値を呼び出し｜呼び出せなかったら0を設定
+  final ret = valToMode(pref.getInt('theme_mode') ?? 0);
+  return ret;
+}
