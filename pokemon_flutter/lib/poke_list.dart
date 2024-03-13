5a15cc6d94253186366dcaf270af7200e3d7fb64
小林正季 <>
2024-03-13 21:47:48 +0900

    poke_listに関する内容を、別ファイルとして切り出し

---
+++ pokemon_flutter/lib/poke_list.dart
@@ -0,0 +1,58 @@
+import '../utils/utils.dart';
+
+import './poke_detail.dart';
+
+// PokemonListを表示するためのクラス
+class PokeList extends StatelessWidget {
+  PokeList({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+      itemCount: 1010,
+      itemBuilder: (context, index) => PokeListItem(index: index),
+    );
+  }
+}
+
+
+// Listから詳細画面に遷移させる
+class PokeListItem extends StatelessWidget {
+  const PokeListItem({Key? key, required this.index}) : super(key: key);
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        width: 80,
+        decoration: BoxDecoration(
+          color: Colors.yellow.withOpacity(.5),
+          borderRadius: BorderRadius.circular(10),
+          image: const DecorationImage(
+              fit: BoxFit.fitWidth,
+              image: NetworkImage(
+                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+              )
+          ),
+        ),
+      ),
+      title: const Text(
+        'pikachu',
+        style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold
+        ),
+      ),
+      subtitle: const Text(
+          '⚡️electric'
+      ),
+      onTap: () => {
+        Navigator.of(context).push(
+            MaterialPageRoute(
+                builder: (BuildContext context) => const PokeDetail()
+            )
+        )
+      },
+    );
+  }
+}
