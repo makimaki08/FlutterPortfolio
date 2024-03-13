f6244ba25993ff38a3d197896feeb8bd2c1a2ff2
小林正季 <>
2024-03-13 21:53:18 +0900

    import文の修正と、メモを追記

--- pokemon_flutter/lib/main.dart
+++ pokemon_flutter/lib/main.dart
@@ -1,8 +1,16 @@
 import 'utils/utils.dart';
-
-import './poke_detail.dart';
 import 'utils/manage_mode.dart';
+
 import './top_page.dart';
+
+/*
+  ChangeNotifierProviderの利用方法って本当に会っている？
+  →サンプルDemoと見比べて、違いを確認
+    →おそらくmain関数の呼び出しは少し修正するくらい
+    →Provider.ofで変更した値をmainに渡す処理が正しく動作していないのでは？
+  →MyAppと、MyHomePageって、2つに別れているけど分ける必要あるの？
+    →同じような宣言繰り返しているだけで、本当は意味ないのでは？
+ */
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
