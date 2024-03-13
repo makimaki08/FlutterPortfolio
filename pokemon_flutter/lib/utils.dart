0bdd3662b973660c5dcc63102dd88410409494ce
小林正季 <>
2024-03-13 21:29:32 +0900

    shared_preferencesプラグインを追加

--- pokemon_flutter/lib/utils.dart
+++ pokemon_flutter/lib/utils.dart
@@ -1,3 +1,4 @@
 export 'package:flutter/material.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 export 'package:provider/provider.dart';
+export 'package:shared_preferences/shared_preferences.dart';
