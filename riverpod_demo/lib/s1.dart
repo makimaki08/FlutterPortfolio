cefbfd2dea08bddbeedf01c978191dd377988902
小林正季 <>
2024-03-25 21:19:40 +0900

    Notifierウィジェットを作成

---
+++ riverpod_demo/lib/s1.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 's1.g.dart';
+
+@riverpod
+class S1Notifier extends _$S1Notifier {
+  @override
+  int build() {
+    // 初期データ
+    return 0;
+  }
+
+  void updateState() {
+    // 変更前のデータ
+    final oldState = State;
+    // 変更後のデータ
+    final newState = oldState + 1;
+    // データを上書き
+    state = newState;
+  }
+}
