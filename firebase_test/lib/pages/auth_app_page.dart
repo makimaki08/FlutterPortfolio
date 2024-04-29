5c1999b18004df87acdd0c24b920f28cd5929d9c
小林正季 <>
2024-04-29 12:32:41 +0900

    スマホ画面における、画面幅を調整

--- firebase_test/lib/pages/auth_app_page.dart
+++ firebase_test/lib/pages/auth_app_page.dart
@@ -17,34 +17,89 @@ class AuthAppPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(authAppPageProvider);
+    // 画面幅を取得
+    double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: AppBar(title: Text(title)),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(20.0),
+          padding: EdgeInsets.all(40.0),
           child: Center(
+            widthFactor: screenWidth * 0.8,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  'OREFFERENCES ACCESS.',
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.w500,
+                // メールアドレス入力
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'メールアドレス'),
+                  onChanged: (String value) {
+                    // setState(() {
+                    //   email = value;
+                    // });
+                  },
+                ),
+                // パスワード入力
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'パスワード'),
+                  obscureText: true,
+                  onChanged: (String value) {
+                    // setState(() {
+                    //   password = value;
+                    // });
+                  },
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  // メッセージ表示
+                  // child: Text(infoText),
+                ),
+                Container(
+                  width: double.infinity,
+                  // ユーザー登録ボタン
+                  child: ElevatedButton(
+                    child: Text('ユーザー登録'),
+                    onPressed: () async {
+                      // try {
+                      //   // メール/パスワードでユーザー登録
+                      //   final FirebaseAuth auth = FirebaseAuth.instance;
+                      //   await auth.createUserWithEmailAndPassword(
+                      //     email: email,
+                      //     password: password,
+                      //   );
+                      //   // ユーザー登録に成功した場合
+                      //   // チャット画面に遷移＋ログイン画面を破棄
+                      //   await Navigator.of(context).pushReplacement(
+                      //     MaterialPageRoute(builder: (context) {
+                      //       return ChatPage();
+                      //     }),
+                      //   );
+                      // } catch (e) {
+                      // ユーザー登録に失敗した場合
+                      // setState(() {
+                      //   infoText = "登録に失敗しました：${e.toString()}";
+                      // });
+                      // }
+                    },
                   ),
-                ),
-                const Gap(10),
-                Text(
-                  state.text,
-                  style: TextStyle(fontSize: 24),
-                ),
-                const Gap(10),
-                ElevatedButton(
-                  onPressed: () =>
-                      ref.read(authAppPageProvider.notifier).fire(),
-                  child: const Text('Button'),
-                ),
+                )
+                // Text(
+                //   'OREFFERENCES ACCESS.',
+                //   style: TextStyle(
+                //     fontSize: 32,
+                //     fontWeight: FontWeight.w500,
+                //   ),
+                // ),
+                // const Gap(10),
+                // Text(
+                //   state.text,
+                //   style: TextStyle(fontSize: 24),
+                // ),
+                // const Gap(10),
+                // ElevatedButton(
+                //   onPressed: () =>
+                //       ref.read(authAppPageProvider.notifier).fire(),
+                //   child: const Text('Button'),
               ],
             ),
           ),
