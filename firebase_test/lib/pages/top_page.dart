0e23b46d078018f9c4890fbae96362c823b1e43f
小林正季 <>
2024-05-04 08:36:00 +0900

    現在のログイン状態を取得する処理を追加

--- firebase_test/lib/pages/top_page.dart
+++ firebase_test/lib/pages/top_page.dart
@@ -1,17 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_test/repositories/secure_storage_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class TopPage extends StatelessWidget {
+class TopPage extends HookConsumerWidget {
   TopPage({super.key});
 
+  // ここでログイン中userの情報取得
+  User? user = FirebaseAuth.instance.currentUser;
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Text('Top Page'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Top Page'),
+              Gap(20),
+              ElevatedButton(
+                child: Text('load'),
+                onPressed: () => _isLogin(user),
+              ),
+              Text(user!.uid),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+Future<void> _isLogin(User? user) async {
+  // ログイン中か確認するメソッド
+  if (user != null && !user!.emailVerified) {
+    await user!.sendEmailVerification();
   }
 }
