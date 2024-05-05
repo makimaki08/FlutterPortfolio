731f1d6d5ba91129bddb8740d2eaf33eb6819f81
小林正季 <>
2024-05-05 16:34:16 +0900

    新規登録画面追加

---
+++ firebase_test/lib/pages/registration_page.dart
@@ -0,0 +1,79 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_test/repositories/secure_storage_repository.dart';
+import 'package:firebase_test/style/color/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+
+import '../models/controller/login/login_controller.dart';
+
+class RegistrationPage extends ConsumerWidget {
+  const RegistrationPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final loginController = ref.watch(loginProvider.notifier);
+
+    /* --- コントローラー --- */
+    /// ユーザーID
+    TextEditingController inputUserIdController = TextEditingController();
+
+    /// パスワード
+    TextEditingController inputPasswordController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(title: Text('新規登録ページ')),
+      body: SafeArea(
+        child: Center(
+          child: Card(
+            child: Container(
+              constraints: BoxConstraints.loose(const Size(600, 600)),
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // メールアドレス入力
+                  TextFormField(
+                    controller: inputUserIdController,
+                    decoration: InputDecoration(labelText: 'メールアドレス'),
+                    textInputAction: TextInputAction.next,
+                  ),
+                  Gap(8),
+
+                  // パスワード入力
+                  TextFormField(
+                    controller: inputPasswordController,
+                    decoration: InputDecoration(labelText: 'パスワード'),
+                    obscureText: true,
+                  ),
+                  Gap(16),
+
+                  // 新規登録
+                  Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      child: Text('新規登録'),
+                      onPressed: () async {
+                        await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                          email: inputUserIdController.text,
+                          password: inputPasswordController.text,
+                        );
+                        context.go('/top');
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
