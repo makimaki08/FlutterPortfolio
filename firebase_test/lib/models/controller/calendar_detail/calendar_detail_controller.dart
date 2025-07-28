3b927ca84eb5a5d77105c03b6269b4e99944a912
小林正季 <>
2025-07-28 22:24:00 +0900

    UXを修正し、コメントをつけて対応内容を整理

--- firebase_test/lib/models/controller/calendar_detail/calendar_detail_controller.dart
+++ firebase_test/lib/models/controller/calendar_detail/calendar_detail_controller.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_test/models/controller/calendar_detail/calendar_detail_state.dart';
 import 'package:firebase_test/models/entities/event/calendar_event.dart';
+import 'package:firebase_test/widgets/loading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -11,11 +12,16 @@ class CalendarDetailController {
   final Ref _ref;
 
   Future<void> addAttendanceInfo(CalendarEvent event, String uid) async {
+    Loading.show();
     DateTime now = DateTime.now();
+    // FIXME: uidがnullになっているので、その対応を追加する。
     final prefs = await SharedPreferences.getInstance();
     final userUid = prefs.getString('user_id');
 
+    // FIXME: この場合だとuidは単体の場合しか対応できず、2名以上を一緒のタイミングで登録することができない。
+    // for文を回すなどして、複数のuidを登録できるようにする必要がある。
     FirebaseFirestore.instance.collection('collectionPath').add(
+      // FIXME: 子供のuidを登録する必要あり。
       {
         'id': event.id,
         'summary': event.summary,
@@ -27,5 +33,6 @@ class CalendarDetailController {
         'uploadTime': now,
       },
     );
+    Loading.dismiss();
   }
 }
--- firebase_test/lib/pages/004_calendar/002_detail/calendar_detail_page.dart
+++ firebase_test/lib/pages/004_calendar/002_detail/calendar_detail_page.dart
@@ -10,6 +10,7 @@ import 'package:firebase_test/widgets/calendar_event_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class CalendarDetailPage extends HookConsumerWidget {
@@ -152,6 +153,7 @@ class CalendarDetailPage extends HookConsumerWidget {
                         const SnackBar(content: Text('欠席を登録しました')),
                       );
                     }
+                    context.pop();
                   },
                   icon: const Icon(
                     Icons.cancel,
